b421631c99247325fb4c9df1dfb2b86f3d6d1129
khouloud <khouloud.abid@esprit.tn>
2022-11-06 14:43:48 -0800

    teste 1tutorial git

--- connection.cpp
+++ connection.cpp
@@ -8,9 +8,9 @@ Connection::Connection()
 bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("source_projet2A");
-db.setUserName("roua");//inserer nom de l'utilisateur
-db.setPassword("admin");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("projet2A");
+db.setUserName("khouloud");//inserer nom de l'utilisateur
+db.setPassword("base123");//inserer mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
